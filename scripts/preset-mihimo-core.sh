0d54f641f6de2facfc69a8a81b87cb943ddca190
grandway2025 <zouchanggan@gmail.com>
2025-07-27 15:10:21 +0800

    Update preset-mihimo-core.sh

--- scripts/preset-mihimo-core.sh
+++ scripts/preset-mihimo-core.sh
@@ -5,7 +5,7 @@ mkdir -p files/etc/openclash/core
 CLASH_META_URL="https://github.com/vernesong/mihomo/releases/download/Prerelease-Alpha/mihomo-linux-amd64-alpha-smart-5bc3f7d.gz"
 GEOIP_URL="https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat"
 GEOSITE_URL="https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat"
-MODEL_URL="https://github.com/vernesong/mihomo/releases/download/LightGBM-Model/model.bin"
+MODEL_URL="https://github.com/vernesong/mihomo/releases/download/LightGBM-Model/model-large.bin"
 
 wget -qO- $CLASH_META_URL | gzip -d > files/etc/openclash/core/mihomo-linux-amd64
 wget -qO- $GEOIP_URL > files/etc/openclash/GeoIP.dat
