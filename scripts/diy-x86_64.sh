7e9142ad1e497f9195c22c919f6c4a4ca9044366
grandway2025 <zouchanggan@gmail.com>
2025-07-27 11:45:07 +0800

    Update diy-x86_64.sh

--- scripts/diy-x86_64.sh
+++ scripts/diy-x86_64.sh
@@ -333,7 +333,7 @@ git clone https://$github/sbwml/feeds_packages_net_aria2 -b 22.03 feeds/packages
 
 # SSRP & Passwall
 rm -rf feeds/packages/net/{xray-core,v2ray-core,v2ray-geodata,sing-box}
-git clone -b openwrt-24.10 https://$gitea/openwrt_helloworld package/new/helloworld
+git clone https://$github/grandway2025/openwrt_helloworld package/new/helloworld
 
 # alist
 rm -rf feeds/packages/net/alist feeds/luci/applications/luci-app-alist
