6dd87f50606644b1bcf6b1e2ea28b567bf9573c3
grandway2025 <zouchanggan@gmail.com>
2025-08-10 15:45:28 +0800

    Update diy-x86_64.sh

--- scripts/diy-x86_64.sh
+++ scripts/diy-x86_64.sh
@@ -336,26 +336,8 @@ rm -rf feeds/packages/net/aria2
 git clone https://$github/sbwml/feeds_packages_net_aria2 -b 22.03 feeds/packages/net/aria2
 
 # SSRP & Passwall
-# rm -rf feeds/packages/net/{xray-core,v2ray-core,v2ray-geodata,sing-box}
-# git clone -b openwrt-24.10 https://git.kejizero.online/private/openwrt_helloworld package/new/helloworld
-
-# Git稀疏克隆，只克隆指定目录到本地
-function git_sparse_clone() {
-  branch="$1" repourl="$2" && shift 2
-  git clone --depth=1 -b $branch --single-branch --filter=blob:none --sparse $repourl
-  repodir=$(echo $repourl | awk -F '/' '{print $(NF)}')
-  cd $repodir && git sparse-checkout set $@
-  mv -f $@ ../package
-  cd .. && rm -rf $repodir
-}
-
-# 科学上网插件
 rm -rf feeds/packages/net/{xray-core,v2ray-core,v2ray-geodata,sing-box}
-git clone --depth=1 -b master https://github.com/fw876/helloworld package/luci-app-ssr-plus
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2 package/luci-app-passwall2
-git_sparse_clone v5 https://github.com/sbwml/openwrt_helloworld luci-app-openclash luci-app-homeproxy luci-app-nikki nikki
+git clone -b openwrt-24.10 https://github.com/grandway2025/helloworld package/new/helloworld
 
 # alist
 rm -rf feeds/packages/net/alist feeds/luci/applications/luci-app-alist
